d0cea2d735cf26e86221ba26de410b7333083f38
DeyanTopalov <deyan.topalow@gmail.com>
2024-03-08 00:33:19 +0200

    setup pt2

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,9 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-clr-gray-100 grid h-svh w-svw place-items-center overflow-hidden md:h-screen md:w-full">
         <main
-          className={`${plusJakartaSans.className}max-w-[90rem] px-4 text-center antialiased md:px-6`}
+          className={`${plusJakartaSans.className}max-w-[90rem] px-4 text-center text-base antialiased md:px-6`}
         >
           {children}
         </main>
